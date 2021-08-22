5b30cb8e8819284b9fba9056a30d4b125905a64e
sskcvqikf <antonapraks@gmail.com>
2021-08-22 19:16:50 +0300

    progress: some tests added

--- main.cc
+++ main.cc
@@ -1,16 +1,30 @@
 #include "pd/pdargs.hh"
 
+#define ASSERT(expr, message)                           \
+    std::cout << "RUNNING: " << #expr;                  \
+    if (!(expr))                                          \
+        std::cerr << "- FAILED\n" << __FILE__ << ":"    \
+            << __LINE__ << " " << message << '\n';      \
+    std::cout << " - PASSED\n";
 
-int main(int argc, char** argv)
+void test1()
 {
-    pd::pdargs pdarg(argc, argv);
-    std::cout << pdarg.get<int>(std::make_pair("port", 'p')).value() << '\n';
-    std::cout << pdarg.get<bool>(std::make_pair("serial", 's')).value() << '\n';
-    std::cout << pdarg.get<bool>(std::make_pair("foo", 'f')).value() << '\n';
-    std::cout << pdarg.get<bool>(std::make_pair("bar", 'b')).value() << '\n';
-    std::cout << pdarg.get<bool>(std::make_pair("sel", 'S')).value() << '\n';
-    std::cout << pdarg.get<double>(std::make_pair("part", 'P')).value() << '\n';
-    std::cout << pdarg.get<unsigned>(std::make_pair("number", 'n')).value() << '\n';
-    pdarg.print();
+    std::cout << " ----- test1 START ----- \n";
+    char* argv[] = {"./main", "--port", "8080", "-Syu"};
+    int argc = 4;
+    pd::pdargs args{argc, argv};
+    ASSERT(args.get<int>({"port", 'p'}).value() == 8080, "port option must be equal to 8080");
+    ASSERT(args.get<bool>({"sus", 'S'}), "port option must be equal to 8080");
+    ASSERT(args.get<bool>({"yuko", 'y'}), "port option must be equal to 8080");
+    ASSERT(args.get<bool>({"update", 'u'}), "port option must be equal to 8080");
+    ASSERT(!args.get<bool>({"increase", 'i'}), "port option must be equal to 8080");
+    ASSERT(!args.get<bool>({"sus", 's'}), "port option must be equal to 8080");
+    std::cout << " ----- test1 PASSED ----- \n";
+}
+
+int main()
+{
+    test1();
+    
     return 0;
 }

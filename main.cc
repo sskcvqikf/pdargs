027f7a066b1ebaa13684f18d27970e63a0f5ced5
sskcvqikf <antonapraks@gmail.com>
2021-08-31 14:25:18 +0300

    some tests added

--- main.cc
+++ main.cc
@@ -13,39 +13,49 @@ bool is_failed = false;
     void name(void* = print_testname(#name))            \
 
 #define ASSERT(expr, message)                           \
-    std::cout << "RUNNING: " << #expr;                  \
     if (!(expr)){ is_failed = true;                     \
-        std::cerr << " - FAILED.\n" << __FILE__ << ":"  \
+        std::cerr << "RUNNING: " << #expr               \
+            << " - FAILED.\n" << __FILE__ << ":"        \
             << __LINE__ << " " << message << '\n';}     \
-    else std::cout << " - PASSED.\n";
 
-#define ASSERT_THROW(expr, exc, message)                \
-    std::cout << "RUNNING: " << #expr;                  \
+#define REQUIRE(expr)                                   \
+    if (!(expr)){ is_failed = true;                     \
+        std::cerr << "RUNNING: " << #expr               \
+            << " - FAILED." << __FILE__ << ":"          \
+            << __LINE__ << '\n';}                       \
+
+#define ASSERT_THROW(expr, exc)                         \
     try {                                               \
         expr;                                           \
         is_failed = true;                               \
-        std::cerr << " - FAILED." << " No exception.\n" \
-        << __FILE__ << ":" << __LINE__ << " "           \
-        << message << '\n';                             \
-    } catch (exc&) {std::cout << " - PASSED.\n";}       \
-      catch (...) { is_failed = true;                   \
-          std::cerr << " - FAILED."                     \
-          << " Other exception.\n" << __FILE__ << ":"   \
-          << __LINE__ << " " << message << '\n';};      \
-
+        std::cerr << "RUNNING: " << #expr               \
+            << " - FAILED." << " No exception. "        \
+            << __FILE__ << ":" << __LINE__ << '\n';     \
+    } catch (exc&) {}                                   \
+      catch (const std::exception& e) {                 \
+            is_failed = true;                           \
+            std::cerr << "RUNNING: " << #expr           \
+                << " - FAILED." << " Other exception. " \
+                << __FILE__ <<":"<< __LINE__  << '\n'   \
+                << e.what() << '\n';}                   \
+      catch (...) {                                     \
+            is_failed = true;                           \
+            std::cerr << "RUNNING: " << #expr           \
+                << " - FAILED." << " Other thing. "     \
+                << __FILE__ <<":"<< __LINE__  << '\n';} \
 
 TEST(BaseTest)
 {
     char* argv[] = {"./main", "--port", "8080", "-Syu", "--lines",  "blank"};
     int argc = 6;
     pd::pdargs args{argc, argv};
-    ASSERT(args.get<int>({"port", 'p'}) == 8080, "port option must be equal to 8080");
-    ASSERT(args.get<bool>({"sus", 'S'}), "sus/S must be true");
-    ASSERT(args.get<bool>({"yuko", 'y'}), "yuko/y must be true");
-    ASSERT(args.get<bool>({"update", 'u'}), "update/u must be true");
-    ASSERT(!args.get<bool>({"increase", 'i'}), "increase/i must be false");
-    ASSERT(args.get<std::string_view>({"lines", 'l'}) == "blank", "lines option must be equal to \"blank\"");
-    ASSERT_THROW(args.get<int>({"port", 'p'}).value(), std::bad_optional_access, "option can be obtained only once.");
+    REQUIRE(args.get<int>({"port", 'p'}) == 8080);
+    REQUIRE(args.get<bool>({"sus", 'S'}));
+    REQUIRE(args.get<bool>({"yuko", 'y'}));
+    REQUIRE(args.get<bool>({"update", 'u'}));
+    REQUIRE(!args.get<bool>({"increase", 'i'}));
+    REQUIRE(args.get<std::string_view>({"lines", 'l'}) == "blank");
+    ASSERT_THROW((args.get<int>({"port", 'p'}).value()), std::bad_optional_access);
 }
 
 TEST(TestNumbers)
@@ -53,16 +63,68 @@ TEST(TestNumbers)
     char* argv[] = {"./main", "--port", "8080", "-f", "12.25", "--negate", "-402", "--portion", "-20.24"};
     int argc = 9;
     pd::pdargs args{argc, argv};
-    ASSERT(args.get<int>({"port", 'p'}) == 8080, "port must be equal to 8080");
-    ASSERT(args.get<float>({"float", 'f'}) == 12.25, "float must be equal to 12.25");
-    ASSERT(args.get<long>({"negate", 'n'}) == -402, "negate must be equal to -402");
-    ASSERT(args.get<double>({"portion", 'P'}) == -20.24, "portion must be equal to -20.24");
+    REQUIRE(args.get<int>({"port", 'p'}) == 8080);
+    REQUIRE(args.get<float>({"float", 'f'}) == 12.25);
+    REQUIRE(args.get<long>({"negate", 'n'}) == -402);
+    REQUIRE(args.get<double>({"portion", 'P'}) == -20.24);
+}
+
+TEST(TestBool)
+{
+    char* argv[] = {"./main", "-Syu", "-iSr", "--root"};
+    int argc = 4;
+    pd::pdargs args{argc, argv};
+    REQUIRE(args.get<bool>({"yummy", 'y'}));
+    REQUIRE(args.get<bool>({"update", 'u'}));
+    REQUIRE(args.get<bool>({"install", 'i'}));
+
+    REQUIRE(!args.get<bool>({"update", 'u'}));
+    REQUIRE(!args.get<bool>({"install", 'i'}));
+
+    ASSERT_THROW(args.get<bool>({"root", 'r'}), std::runtime_error);
+    ASSERT_THROW(args.get<bool>({"Sus", 'S'}), std::invalid_argument);
+}
+
+TEST(TestStrings)
+{
+    char* argv[] = {"./main", "--selection", "primary", "--mode", "active", "-f", "soft", "--force", "super"};
+    int argc = 9;
+    pd::pdargs args{argc, argv};
+    REQUIRE(args.get<std::string_view>({"selection", 's'}) == "primary");
+    REQUIRE(args.get<std::string>({"mode", 'm'}) == "active");
+
+    ASSERT_THROW(args.get<std::string_view>({"mode", 'm'}).value(), std::bad_optional_access);
+    ASSERT_THROW(args.get<std::string>({"selection", 's'}).value(), std::bad_optional_access);
+
+    ASSERT_THROW(args.get<std::string_view>({"force", 'f'}), std::runtime_error);
+}
+
+TEST(TestGetOr)
+{
+    char *argv[] = {"./main", "--selection", "primary", "-f", "soft", "-c", "55", "-p", "0.5", "--install"};
+    int argc{10};
+    pd::pdargs args{argc, argv};
+
+    REQUIRE(args.get_or<std::string>({"selection", 's'}, "secondary") == "primary");
+    REQUIRE(args.get_or<std::string>({"model", 'm'}, "new") == "new");
+
+    REQUIRE(args.get_or<int>({"count", 'c'}, 100) == 55);
+    REQUIRE(args.get_or<int>({"number", 'n'}, 128) == 128);
+
+    REQUIRE(args.get_or<double>({"portion", 'p'}, 0.25) == 0.5);
+    REQUIRE(args.get_or<double>({"fraction", 'F'}, 0.75) == 0.75);
+
+    REQUIRE(args.get<bool>({"install", 'i'}));
+    REQUIRE(!args.get<bool>({"update", 'u'}));
 }
 
 int main()
 {
     BaseTest();
     TestNumbers();
+    TestBool();
+    TestStrings();
+    TestGetOr();
     if (is_failed)
         exit(1);
     
